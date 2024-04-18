966efa3218473e262b9bf4b8a29bf3208aa26624
Yousef Abdelnour <2242238@edu.vaniercollege.qc.ca>
2024-04-18 11:33:37 -0400

    Created database

---
+++ maidservice.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 18, 2024 at 05:32 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maidservice`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Account`
+--
+
+CREATE TABLE `Account` (
+  `AccountId` int(11) NOT NULL,
+  `Name` varchar(1000) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password_Hash` varchar(60) NOT NULL,
+  `Phone_Number` varchar(10) NOT NULL,
+  `Status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Address`
+--
+
+CREATE TABLE `Address` (
+  `AddressId` int(11) NOT NULL,
+  `AccountId` int(11) NOT NULL,
+  `Building_Number` varchar(10) NOT NULL,
+  `Street_Name` varchar(50) NOT NULL,
+  `ZipCode` varchar(10) NOT NULL,
+  `State` varchar(50) NOT NULL,
+  `Country` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Job`
+--
+
+CREATE TABLE `Job` (
+  `JobId` int(11) NOT NULL,
+  `AddressId` int(11) NOT NULL,
+  `Time_Of_Job` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Status` tinyint(4) NOT NULL,
+  `House_Size` tinyint(4) NOT NULL,
+  `Spots_Left` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Payment_Log`
+--
+
+CREATE TABLE `Payment_Log` (
+  `Payment_Id` int(11) NOT NULL,
+  `AccountId` int(11) NOT NULL,
+  `Confirmation_Number` varchar(50) NOT NULL,
+  `Time_Stamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Account`
+--
+ALTER TABLE `Account`
+  ADD PRIMARY KEY (`AccountId`),
+  ADD UNIQUE KEY `Username_Account_Unique` (`Username`);
+
+--
+-- Indexes for table `Address`
+--
+ALTER TABLE `Address`
+  ADD PRIMARY KEY (`AddressId`),
+  ADD KEY `Address_To_Account_FK` (`AccountId`);
+
+--
+-- Indexes for table `Job`
+--
+ALTER TABLE `Job`
+  ADD PRIMARY KEY (`JobId`),
+  ADD KEY `Job_To_Address_FK` (`AddressId`);
+
+--
+-- Indexes for table `Payment_Log`
+--
+ALTER TABLE `Payment_Log`
+  ADD PRIMARY KEY (`Payment_Id`),
+  ADD KEY `Payment_To_Account_FK` (`AccountId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Account`
+--
+ALTER TABLE `Account`
+  MODIFY `AccountId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Address`
+--
+ALTER TABLE `Address`
+  MODIFY `AddressId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Job`
+--
+ALTER TABLE `Job`
+  MODIFY `JobId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Payment_Log`
+--
+ALTER TABLE `Payment_Log`
+  MODIFY `Payment_Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Address`
+--
+ALTER TABLE `Address`
+  ADD CONSTRAINT `Address_To_Account_FK` FOREIGN KEY (`AccountId`) REFERENCES `Account` (`AccountId`);
+
+--
+-- Constraints for table `Job`
+--
+ALTER TABLE `Job`
+  ADD CONSTRAINT `Job_To_Address_FK` FOREIGN KEY (`AddressId`) REFERENCES `Address` (`AddressId`);
+
+--
+-- Constraints for table `Payment_Log`
+--
+ALTER TABLE `Payment_Log`
+  ADD CONSTRAINT `Payment_To_Account_FK` FOREIGN KEY (`AccountId`) REFERENCES `Account` (`AccountId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
