a0df8b564f3c6e5d046280d9e51cd4451f467f60
Dieselfrl <cristian.andrei.gasper@gmail.com>
2024-04-20 16:12:57 -0400

    Navbar
    navbar is created

---
+++ app/views/navbar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <input type="checkbox" id="menu-toggle" class="menu-toggle" />
+        <label for="menu-toggle" class="hamburger">&#9776;</label>
+        <ul id="menu">
+            <li><a href="/home.php">Home</a></li>
+            <li><a href="/about.php">Book</a></li>
+            <li><a href="/contact.php">Contact Us</a></li>
+        </ul>
+    </nav>
+    <div id="nav_background"></div>
+</body>
+
+</html>
