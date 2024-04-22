d379ba9c7c8b1abb7e99a1b7f77b0c82ce077a51
Rowan Lajoie <2179433@edu.vaniercollege.qc.ca>
2024-04-22 13:15:29 -0400

    Added controller folder and job.php

---
+++ app/controllers/Job.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace app\controllers; 
+
+class Job extends \app\core\Controller 
+{
+
+    public function index(){
+
+        $jobModel = new \app\models\Job(); 
+        
+        
+
+    }
+
+
+}
